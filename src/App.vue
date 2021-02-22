32282ba49105ab4454e958a70099a4508824da24
NarathSpring <valentine.xu@yahoo.com>
2021-02-22 18:49:50 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ChargingMobile />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import ChargingMobile from "./components/ChargingMobile.vue";
 
 @Component({
   components: {
-    HelloWorld
+    ChargingMobile
   }
 })
 export default class App extends Vue {}
@@ -24,6 +23,6 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/ChargingMobile.vue
@@ -0,0 +1,41 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions" class="swiper-container">
+    <swiper-slide class="slide1">1111111111111111</swiper-slide>
+    <swiper-slide class="slide2">22222222222222222</swiper-slide>
+    <swiper-slide class="slide3">33333333333333333</swiper-slide>
+  </swiper>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+import { SwiperOptions } from "swiper";
+
+export default class ChargingMobile extends Vue {
+  swiperOptions: SwiperOptions = {
+    effect: "fade",
+    direction: "vertical",
+    speed: 300,
+    freeMode: false
+  };
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+swiper-slide {
+  height: 300px;
+}
+.slide1 {
+  background: blue;
+}
+.slide2 {
+  background: red;
+}
+.slide3 {
+  background: yellow;
+}
+.swiper-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
